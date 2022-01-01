781be5bd98bdab0e728da73603c984fdba728539
Kent Waxman <kawaxman@icloud.com>
2021-12-31 23:51:40 -0700

    Update BTC_MACD_Script.py

--- BTC_MACD_Script.py
+++ BTC_MACD_Script.py
@@ -4,9 +4,9 @@ import pandas as p
 import robinhood_helper as rh
 #constants
 BTC_MIN_PURCHASE_SELL_AMOUNT = 100.00
-EMA_LOWER_VALUE = 20
-EMA_UPPER_VALUE = 40
-FMA_SIGNAL_VALUE = 4
+EMA_LOWER_VALUE = 12
+EMA_UPPER_VALUE = 26
+FMA_SIGNAL_VALUE = 9
     
 #get pricing information
 def getMACDTrendDifference():
